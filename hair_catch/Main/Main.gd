763ace4b0b06fddfc5c1340a7836396156712635
MMerisalu <merilyn.merisalu.01@gmail.com>
2024-01-27 17:07:31 +0200

    Clock color changed to red!

--- hair_catch/Main/Main.gd
+++ hair_catch/Main/Main.gd
@@ -1,13 +1,13 @@
 extends Node
-
 var default_minutes :int = 1;
 var default_seconds : int = 30;
 var minutes= 0;
 var seconds = 0;
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	_reset_timer();
-	
+	$GameTimerLabel.modulate = Color.BLACK
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass	
@@ -23,8 +23,9 @@ func _on_game_timer_timeout() -> void:
 			minutes -= 1;
 			seconds = 60;
 	seconds -= 1;
-	if (minutes == 0 and (seconds <= 10)):
+	if minutes == 0 and seconds <= 10:
 		$Ticking.play();
+		$GameTimerLabel.modulate = Color.RED;
 	if (minutes == 0 and seconds == 0 ):
 		$GameTimer.stop();
 		$Ticking.stop();
