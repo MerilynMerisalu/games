6800879bb42c4fb67895309dfca0970362f21390
MMerisalu <merilyn.merisalu.01@gmail.com>
2024-01-30 22:48:31 +0200

    Version 1 of UI!

--- hair_catch/Main/Main.gd
+++ hair_catch/Main/Main.gd
@@ -1,13 +1,13 @@
 extends Node
 var default_minutes :int = 1;
-var default_seconds : int = 30;
+var default_seconds : int = 31;
 var minutes= 0;
 var seconds = 0;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	_reset_timer();
-	$GameTimerLabel.modulate = Color.BLACK
+	$TimerContainer/GameTimerLabel.modulate = Color.BLACK
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass	
@@ -25,11 +25,11 @@ func _on_game_timer_timeout() -> void:
 	seconds -= 1;
 	if minutes == 0 and seconds <= 10:
 		$Ticking.play();
-		$GameTimerLabel.modulate = Color.RED;
+		$TimerContainer/GameTimerLabel.modulate = Color.RED;
 	if (minutes == 0 and seconds == 0 ):
 		$GameTimer.stop();
 		$Ticking.stop();
-	$GameTimerLabel.text = str(minutes) + ":" + str(seconds);
+	$TimerContainer/GameTimerLabel.text = str(minutes) + ":" + str(seconds);
 		
 
 
