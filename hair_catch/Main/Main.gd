dc882e74a221b822cc34f47195e06cbebd4e1a73
MMerisalu <merilyn.merisalu.01@gmail.com>
2024-02-01 18:57:14 +0200

    Changing window mode working!

--- hair_catch/Main/Main.gd
+++ hair_catch/Main/Main.gd
@@ -9,8 +9,8 @@ func _ready():
 	_reset_timer();
 	$TimerContainer/GameTimerLabel.modulate = Color.BLACK
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass	
+func _process(_delta):
+		_change_window_mode();	
 	
 func _reset_timer() -> void:
 	minutes = default_minutes;
@@ -34,4 +34,9 @@ func _on_game_timer_timeout() -> void:
 
 
 func _on_link_button_pressed() -> void:
-	OS.shell_open("https://www.zapsplat.com/")
+	OS.shell_open("https://www.zapsplat.com/");
+func _change_window_mode() -> void:
+	if Input.is_action_pressed("ui_cancel"):
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED);
+	elif Input.is_action_pressed("full_screen"):
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN);	
