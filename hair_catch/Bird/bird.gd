989a0a34ac400bfdc85671990494596963e0a016
MMerisalu <merilyn.merisalu.01@gmail.com>
2024-02-05 16:11:00 +0200

    small fixes!

--- hair_catch/Bird/bird.gd
+++ hair_catch/Bird/bird.gd
@@ -4,7 +4,6 @@ extends CharacterBody2D
 func _ready() -> void:
 	position = Vector2(137, 429);
 	
-
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		position = get_global_mouse_position();
