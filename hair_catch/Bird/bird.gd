d27d8eec9b2d91f97caaa0e88c861b630177855d
MMerisalu <merilyn.merisalu.01@gmail.com>
2024-02-09 15:13:56 +0200

    Start screen version 1!

--- hair_catch/Bird/bird.gd
+++ hair_catch/Bird/bird.gd
@@ -7,5 +7,8 @@ func _ready() -> void:
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		position = get_global_mouse_position();
-		$AnimatedSprite2D.animation = "fly";
-		$AnimatedSprite2D.play();
+		
+
+
+
+
--- hair_catch/screen.gd
+++ hair_catch/screen.gd
@@ -2,7 +2,7 @@ extends Node
 
 var screen_size: Vector2;
 var window_mode;
-const SCREEN_CONSTRAINT : int = 55;
+#const SCREEN_CONSTRAINT : int = 55;
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	screen_size = get_window().size;
@@ -12,4 +12,10 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	change_window_mode()
+	
+func change_window_mode() -> void:
+	if Input.is_action_pressed("ui_cancel"):
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED);
+	elif Input.is_action_pressed("full_screen"):
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN);	
