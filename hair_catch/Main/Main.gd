5f7e69cbb293fdcc4ef28e3cb2f744c4f17f0eab
MMerisalu <merilyn.merisalu.01@gmail.com>
2024-03-02 21:48:38 +0200

    Creating man scene!

--- hair_catch/Main/Main.gd
+++ hair_catch/Main/Main.gd
@@ -1,20 +1,25 @@
 extends Node
+
+
+const MAN_SCENE : PackedScene = preload("res://Man/Man.tscn");
+@onready var GAMETIMER : Timer = get_node("GameTimer");
+@onready var COUNTING: AudioStreamPlayer = get_node("LevelContainer/Counting");
 var default_minutes :int = 1;
 var default_seconds : int = 31;
-var minutes= 0;
+var minutes = 0;
 var seconds = 0;
-
+var hair_left : int = 5;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$LevelContainer/StartTimerContainer/StartLabel.visible = false;
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN);
-	
 	_reset_timer()
 	$ColorRect/UI/TimerContainer/GameTimerLabel.modulate = Color.BLACK;
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 		Screen.change_window_mode();
+		
 	
 func _reset_timer() -> void:
 	minutes = default_minutes;
@@ -40,13 +45,8 @@ func _on_game_timer_timeout() -> void:
 func _on_link_button_pressed() -> void:
 	OS.shell_open("https://www.zapsplat.com/");
 	
-
-
-#func _on_start_timer_timeout() -> void:
-#	$StartTimerContainer/StartLabel.visible = false;
-
-#func _on_level_timer_timeout() -> void:
-	#$LevelContainer/Level.visible = false;
-	#$StartTimerContainer/StartLabel.visible = true;
-	#$StartTimer.start();
-	#$Counting.play();
+func _on_create_men_timer_timeout() -> void:
+	if (COUNTING.finished) or (minutes != 0 and seconds != 0) or (hair_left > 0):
+		var man = MAN_SCENE.instantiate();
+		$Men.add_child(man);
+	
