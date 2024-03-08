11ac54899431ea7bf48d5ed07c18ef9868e1ecdc
MMerisalu <merilyn.merisalu.01@gmail.com>
2024-03-09 01:56:07 +0200

    First version of interaction between the bird and the man

--- hair_catch/Man/Man.gd
+++ hair_catch/Man/Man.gd
@@ -1,11 +1,13 @@
 extends RigidBody2D
 
+const CRY_IMG : Texture = preload("res://assets/characters/man/cry.png");
 var screen_size;
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	screen_size = Screen.screen_size;
 	position.x = randf_range(0, Screen.screen_size.x - Screen.SCREEN_CONSTRAINT )
-
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
@@ -13,3 +15,7 @@ func _process(_delta: float) -> void:
 
 
 
+
+func _on_body_entered(_body: Node) -> void:
+	$"Sprite2D".texture = CRY_IMG;
+	$Pain.play();
