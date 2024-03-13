a6cf9352c1c65feacd2e8f59bb1fae5fe6389e21
MMerisalu <merilyn.merisalu.01@gmail.com>
2024-03-14 00:42:11 +0200

    small fixes!

--- hair_catch/UI/level.gd
+++ hair_catch/UI/level.gd
@@ -2,10 +2,11 @@ extends BoxContainer
 
 signal start_label_finished;
 
+var level : int = 1
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Level.visible = true;
-
+	$Level.text = "Level " + str(level);
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
