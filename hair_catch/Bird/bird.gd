65b20a80af0e355f0290a57b7505b3f91961a714
MMerisalu <merilyn.merisalu.01@gmail.com>
2024-03-29 17:22:40 +0200

    bird movement animation fixed!

--- hair_catch/Bird/bird.gd
+++ hair_catch/Bird/bird.gd
@@ -2,16 +2,20 @@ extends CharacterBody2D
 
 signal hit;
 var can_move: bool = false;
-
+var timer_delay : float = 0.1;
 func _ready() -> void:
 	position = Vector2(137, 429);
 	EventBus.bird_can_move.connect(move);
 	
-
+	
 func _input(event: InputEvent) -> void:
 	if(can_move == true):
 		if (event is InputEventMouseMotion):
-			position = get_global_mouse_position();
+			$AnimatedSprite2D.play("fly")
+			position = get_global_mouse_position()
+			$AnimationTimer.stop()
+			$AnimationTimer.start(timer_delay)
+		$AnimationTimer.start();
 		
 
 func move() -> void:
@@ -24,5 +28,5 @@ func move() -> void:
 			EventBus.is_level_passed = false;
 		
 
-
-	
+func _on_animation_timer_timeout() -> void:
+	$AnimatedSprite2D.stop();
