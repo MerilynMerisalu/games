3fb3e3dacc5f8c575a9863b4e2235db692ebb048
MMerisalu <merilyn.merisalu.01@gmail.com>
2024-03-30 18:05:07 +0200

    small fixes!

--- hair_catch/UI/level.gd
+++ hair_catch/UI/level.gd
@@ -23,13 +23,13 @@ func _ready() -> void:
 	hair_left_label.text = str(hair_left) + EventBus.HAIR_LEFT;
 	_on_display_level_changed();
 	_reset_timer();
-	_on_level_container_start_label_finished()
-	EventBus.connect("hair_caught", _on_hair_caught)
-	EventBus.connect("level_up", _on_level_changed)
+	_on_level_container_start_label_finished();
+	EventBus.connect("hair_caught", _on_hair_caught);
+	EventBus.connect("level_up", _on_level_changed);
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	game_over()
+	game_over();
 	
 
 func _on_level_timer_timeout() -> void:
