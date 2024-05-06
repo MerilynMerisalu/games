75fe42e6845075a796a5038a5fde906f1f4819fb
MMerisalu <merilyn.merisalu.01@gmail.com>
2024-05-06 16:35:47 +0300

    Base things

--- hair_catch/UI/UI.gd
+++ hair_catch/UI/UI.gd
@@ -3,7 +3,6 @@ extends base_ui
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
 	EventBus.level_up.connect(_on_life_show);
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
