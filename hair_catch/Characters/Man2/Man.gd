593740ca2525c9f06ce2b921fb5029bb65c802c2
MMerisalu <merilyn.merisalu.01@gmail.com>
2024-05-18 14:09:15 +0300

    Man movement done

--- hair_catch/Characters/Man2/Man.gd
+++ hair_catch/Characters/Man2/Man.gd
@@ -1,34 +1,36 @@
 extends Area2D
 
-const CHANGED_BY : int = 530;
-var has_timer_started : bool = false;
+
+
+const UPPER_BOUNDARY : int = 550;
+const MOVE_AMOUNT : int = 10;
+var has_appeared : bool = false;
+var is_up : bool = false;
+var is_down : bool = true;
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	position.x = randf_range(0, Screen.screen_size.y);
+	position.x = randf_range(0, Screen.screen_size.x);
 	position.y = Screen.screen_size.y;
-	print(position.y)
-		
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if($VisibleOnScreenNotifier2D.is_on_screen() == false \
-	 and has_timer_started == false):
-		position.y -= CHANGED_BY;
-		
-
-
-
-
-func _on_visible_on_screen_notifier_2d_screen_entered() -> void:
-	$VisibilityTimer.start()
 	
 
 
-func _on_visibility_timer_timeout() -> void:
-	position.y += CHANGED_BY;
-	has_timer_started = true;
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	if (has_appeared == false):
+		if(position.y > (Screen.screen_size.y - UPPER_BOUNDARY)):
+			position.y -= MOVE_AMOUNT;
+		else:
+			is_up = true;
+			await get_tree().create_timer(3.0).timeout;
+			has_appeared = true;
+			if is_up == true:
+				if(position.y < Screen.screen_size.y):
+					position.y += MOVE_AMOUNT;
+				
+				
 
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
-	queue_free();
+	if(has_appeared == true):
+		queue_free();
